f0d2f6ebea080d866f833a690aa7d17325b6e171
picafern-commits <pica.fern@gmail.com>
2026-03-16 18:27:10 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,13 +8,13 @@ const urlsToCache = [
   '/firebaseConfig.js',
   '/manifest.json'
 ];
- 
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
- 
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
