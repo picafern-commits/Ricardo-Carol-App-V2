eac1c2fab9710d2e305691da8bad3142aacef591
picafern-commits <pica.fern@gmail.com>
2026-03-16 18:14:56 +0000

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// firebaseConfig.js
+const firebaseConfig = {
+apiKey:"AIzaSyCqOp8DRQQI4JZSzOnX_4yl3TrhxT4m6S0",
+authDomain:"ricardo-carol-app.firebaseapp.com",
+projectId:"ricardo-carol-app"
+};
+ 
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
