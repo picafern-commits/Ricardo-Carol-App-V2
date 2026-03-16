f8db6f3a6c7fe0031a45c8552d9035e82f48a7ec
picafern-commits <pica.fern@gmail.com>
2026-03-16 15:29:26 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
 self.addEventListener("install", e => {
-e.waitUntil( caches.open("app").then(cache => { return cache.addAll([ "./", "./index.html" ]) }) )
-})
+  e.waitUntil(
+    caches.open("app-v1").then(cache => {
+      return cache.addAll(["./","./index.html","./manifest.json"]);
+    })
+  );
+});
