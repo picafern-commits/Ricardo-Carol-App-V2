84abd0b24362dc0b3c741355f42526ab8b91ad0c
picafern-commits <pica.fern@gmail.com>
2026-04-13 22:23:44 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE_NAME='shopping-list-pro-v1',FILES=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
